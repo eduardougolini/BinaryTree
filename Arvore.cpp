674b103e6b271eb044d0c55d6f19308220196dfc
Eduardo Ugolini <edu.ugolini2@gmail.com>
2016-05-31 20:13:13 -0300

    Melhoria no código de ordenação de valores na árvore

--- Arvore.cpp
+++ Arvore.cpp
@@ -2,163 +2,167 @@
 #include <stdlib.h>
 
 struct Tree {
-	struct Tree *backPointer;
-	int nodeValue;
-	struct Tree *nextPointerLeft;
-	struct Tree *nextPointerRight;
+    struct Tree *backPointer;
+    int nodeValue;
+    struct Tree *nextPointerLeft;
+    struct Tree *nextPointerRight;
 };
 
 int readRootValue() {
-	int root;
-	
-	printf("Informe o numero da raiz: ");
-	scanf_s("%d", &root);
-	
-	return root;
+    int root;
+
+    printf("Informe o numero da raiz: ");
+    scanf("%d", &root);
+
+    return root;
 }
 
 int readTreeValue() {
-	int treeValue;
+    int treeValue;
 
-	printf("Informe um numero a ser adicionado a estrutura da arvore: (0 cancela execucao, 10000 mostra valores inseridos)");
-	scanf_s("%d", &treeValue);
+    printf("Informe um numero a ser adicionado a estrutura da arvore: (0 cancela execucao, 10000 mostra valores inseridos)");
+    scanf("%d", &treeValue);
 
-	return treeValue;
+    return treeValue;
 }
 
 void addNodeToRight(Tree *parentNode, Tree *childNode) {
-	parentNode->nextPointerRight = childNode;
-	childNode->backPointer = parentNode;
+    parentNode->nextPointerRight = childNode;
+    childNode->backPointer = parentNode;
 }
 
 void addNodeToLeft(Tree *parentNode, Tree *childNode) {
-	parentNode->nextPointerLeft = childNode;
-	childNode->backPointer = parentNode;
+    parentNode->nextPointerLeft = childNode;
+    childNode->backPointer = parentNode;
 }
 
-void orderNode(Tree *mainNode, int treeValue) {
-	Tree* selectedNode = mainNode;
-	Tree* newNode = new Tree();
-	newNode->nodeValue = treeValue;
+void orderNode(Tree *rootNode, int treeValue) {
+    Tree* selectedNode = rootNode;
+    Tree* newNode = new Tree();
+    newNode->nodeValue = treeValue;
 
-	while (true) {
-		if (!selectedNode->nextPointerLeft && selectedNode->nodeValue > treeValue) {
-			addNodeToLeft(selectedNode, newNode);
-			return;
-		}
-
-		if (!selectedNode->nextPointerRight && selectedNode->nodeValue < treeValue) {
-			addNodeToRight(selectedNode, newNode);
-			return;
-		}
-
-		if (selectedNode->nodeValue > treeValue && selectedNode->nextPointerLeft) {
-			selectedNode = selectedNode->nextPointerLeft;
-		}
-
-		if (selectedNode->nodeValue < treeValue && selectedNode->nextPointerRight) {
-			selectedNode = selectedNode->nextPointerRight;
-		}
-
-		return;
-	}
+    while (true) {
+        
+        if (selectedNode->nodeValue == treeValue) {
+            return;
+        } else if (selectedNode->nodeValue > treeValue) {
+            
+            if (selectedNode->nextPointerLeft) {
+                selectedNode = selectedNode->nextPointerLeft;
+            } else {
+                addNodeToLeft(selectedNode, newNode);
+                return;
+            }
+            
+        } else if (selectedNode->nodeValue < treeValue) {
+            
+            if (selectedNode->nextPointerRight) {
+                selectedNode = selectedNode->nextPointerRight;
+            } else {
+                addNodeToRight(selectedNode, newNode);
+                return;
+            }
+            
+        }
+        
+    }
 
 }
 
 void deleteNodeWithDoubleChilds(Tree* selectedNode) {
-	Tree* backupNode = selectedNode;
-	Tree* backNode = selectedNode->backPointer;
+    Tree* backupNode = selectedNode;
+    Tree* backNode = selectedNode->backPointer;
 
-	if (backNode->nextPointerLeft == selectedNode) {
-		backNode->nextPointerLeft = selectedNode->nextPointerLeft;
-	} else {
-		return;
-	}
+    if (backNode->nextPointerLeft == selectedNode) {
+            backNode->nextPointerLeft = selectedNode->nextPointerLeft;
+    } else {
+            return;
+    }
 	
 }
 
 void deleteNodeWithOneChild(Tree* selectedNode, char type) {
-	Tree* backNode = selectedNode->backPointer;
+    Tree* backNode = selectedNode->backPointer;
 
-	if (type == 'l') {
-		backNode->nextPointerLeft = selectedNode->nextPointerRight;
-	} else if (type == 'r') {
-		backNode->nextPointerRight = selectedNode->nextPointerRight;
-	}
+    if (type == 'l') {
+            backNode->nextPointerLeft = selectedNode->nextPointerRight;
+    } else if (type == 'r') {
+            backNode->nextPointerRight = selectedNode->nextPointerRight;
+    }
 }
 
 void deleteNode(Tree* selectedNode) {
-	if (selectedNode->nextPointerLeft && selectedNode->nextPointerRight) {
-		deleteNodeWithDoubleChilds(selectedNode);
-	} else if (selectedNode->nextPointerLeft) {
-		deleteNodeWithOneChild(selectedNode, 'l');
-	} else if (selectedNode->nextPointerRight) {
-		deleteNodeWithOneChild(selectedNode, 'r');
-	}
+    if (selectedNode->nextPointerLeft && selectedNode->nextPointerRight) {
+            deleteNodeWithDoubleChilds(selectedNode);
+    } else if (selectedNode->nextPointerLeft) {
+            deleteNodeWithOneChild(selectedNode, 'l');
+    } else if (selectedNode->nextPointerRight) {
+            deleteNodeWithOneChild(selectedNode, 'r');
+    }
 }
 
 void showInsertedValues(Tree *mainNode) {
-	Tree* selectedNode = mainNode;
+    Tree* selectedNode = mainNode;
 
-	while (true) {
+    while (true) {
 
-		printf("Valor do no atual e %d\n", selectedNode->nodeValue);
+        printf("Valor do no atual e %d\n", selectedNode->nodeValue);
 
-		int hasBackPointer = false;
-		if (! selectedNode->backPointer) {
-			printf("Aperte 4 para ir para o no a esquerda, 6 para ir para a direita ou 0 para sair\n");
-		} else {
-			hasBackPointer = true;
-			printf("Aperte 4 para ir para o no a esquerda, 6 para ir para a direita, 2 para ir ao no anterior ou 0 para sair\n");
-		}
+        int hasBackPointer = false;
+        if (! selectedNode->backPointer) {
+            printf("Aperte 4 para ir para o no a esquerda, 6 para ir para a direita ou 0 para sair\n");
+        } else {
+            hasBackPointer = true;
+            printf("Aperte 4 para ir para o no a esquerda, 6 para ir para a direita, 2 para ir ao no anterior ou 0 para sair\n");
+        }
 
-		int option;
-		scanf_s("%d", &option);
+        int option;
+        scanf("%d", &option);
 
-		if (hasBackPointer && option == 2) {
-			selectedNode = selectedNode->backPointer;
-		} else if (option == 4) {
+        if (hasBackPointer && option == 2) {
+            selectedNode = selectedNode->backPointer;
+        } else if (option == 4) {
 
-			if (!selectedNode->nextPointerLeft) {
-				printf("Nao existe valor a esquerda!");
-				return;
-			}
+            if (!selectedNode->nextPointerLeft) {
+                printf("Nao existe valor a esquerda!");
+                return;
+            }
 
-			selectedNode = selectedNode->nextPointerLeft;
-		} else if (option == 6) {
+            selectedNode = selectedNode->nextPointerLeft;
+        } else if (option == 6) {
 
-			if (!selectedNode->nextPointerRight) {
-				printf("Nao existe valor a direita!");
-				return;
-			}
+            if (!selectedNode->nextPointerRight) {
+                printf("Nao existe valor a direita!");
+                return;
+            }
 
-			selectedNode = selectedNode->nextPointerRight;
-		} else if (option == 0) {
-			return;
-		}
-	}
+            selectedNode = selectedNode->nextPointerRight;
+        } else if (option == 0) {
+            return;
+        }
+    }
 }
 
 int main() {
 	
-	Tree* mainNode = new Tree();
-	mainNode->nodeValue = readRootValue();
+    Tree* rootNode = new Tree();
+    rootNode->nodeValue = readRootValue();
 
-	while (true) {
-		int treeValue = readTreeValue();
+    while (true) {
+        int treeValue = readTreeValue();
 
-		if (treeValue == 0) {
-			break;
-		}
+        if (treeValue == 0) {
+                break;
+        }
 
-		if (treeValue == 10000) {
-			showInsertedValues(mainNode);
-		}
+        if (treeValue == 10000) {
+                showInsertedValues(rootNode);
+        }
 
-		orderNode(mainNode, treeValue);
+        orderNode(rootNode, treeValue);
 
-	}
+    }
 
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
